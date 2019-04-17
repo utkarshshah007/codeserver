1c9102303b08cdc88fbbdb5e78b68031ddb0f1b8
Utkarsh Shah <utkarshshah007@gmail.com>
2019-04-16 20:07:25 -0400

    removing crispy forms

--- codeserver/codeserver/settings.py
+++ codeserver/codeserver/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'codes.apps.CodesConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,9 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'codeserver.wsgi.application'
-
-# Django Crispy Forms
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 # Database
